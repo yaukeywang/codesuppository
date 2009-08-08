cc6a1af188e1a9a56026163445e2ca8072bb9d51
jratcliffscarab <jratcliffscarab@f10ad84c-df75-11dd-bd7b-b12b6590754f>
2009-08-08 20:16:13 +0000

    cleanup

---
+++ include/common/snippets/get.bat
@@ -0,0 +1,44 @@
+copy \p4\TRUNK\APEX\shared\external\include\asc2bin.h
+copy \p4\TRUNK\APEX\shared\external\src\asc2bin.cpp
+REM copy \p4\TRUNK\APEX\shared\external\include\AutoGeometry.h
+REM copy \p4\TRUNK\APEX\shared\external\src\AutoGeometry.cpp
+copy \p4\TRUNK\APEX\shared\external\include\FastXml.h
+copy \p4\TRUNK\APEX\shared\external\src\FastXml.cpp
+copy \p4\TRUNK\APEX\shared\external\include\FileInterface.h
+copy \p4\TRUNK\APEX\shared\external\src\FileInterface.cpp
+copy \p4\TRUNK\APEX\shared\external\include\filesystem.h
+copy \p4\TRUNK\APEX\shared\external\src\filesystem.cpp
+copy \p4\TRUNK\APEX\shared\external\include\FloatMath.h
+copy \p4\TRUNK\APEX\shared\external\src\FloatMath.inl
+copy \p4\TRUNK\APEX\shared\external\src\FloatMath.cpp
+copy \p4\TRUNK\APEX\shared\external\include\inparser.h
+copy \p4\TRUNK\APEX\shared\external\src\inparser.cpp
+copy \p4\TRUNK\APEX\shared\external\include\keyvalue.h
+copy \p4\TRUNK\APEX\shared\external\src\keyvalue.cpp
+copy \p4\TRUNK\APEX\shared\external\include\KeyValueIni.h
+copy \p4\TRUNK\APEX\shared\external\src\KeyValueIni.cpp
+copy \p4\TRUNK\APEX\shared\external\include\MeshConsolidate.h
+copy \p4\TRUNK\APEX\shared\external\src\MeshConsolidate.cpp
+copy \p4\TRUNK\APEX\shared\external\include\MeshImport.h
+copy \p4\TRUNK\APEX\shared\external\src\MeshImport.cpp
+REM copy \p4\TRUNK\APEX\shared\external\include\MeshIslandGeneration.h
+REM copy \p4\TRUNK\APEX\shared\external\src\MeshIslandGeneration.cpp
+REM copy \p4\TRUNK\APEX\shared\external\include\RemoveTjunctions.h
+REM copy \p4\TRUNK\APEX\shared\external\src\RemoveTjunctions.cpp
+copy \p4\TRUNK\APEX\shared\external\include\SendTextMessage.h
+copy \p4\TRUNK\APEX\shared\external\include\stable.h
+copy \p4\TRUNK\APEX\shared\external\include\StanHull.h
+copy \p4\TRUNK\APEX\shared\external\src\StanHull.cpp
+copy \p4\TRUNK\APEX\shared\external\include\stringdict.h
+copy \p4\TRUNK\APEX\shared\external\src\stringdict.cpp
+copy \p4\TRUNK\APEX\shared\external\include\sutil.h
+copy \p4\TRUNK\APEX\shared\external\src\sutil.cpp
+copy \p4\TRUNK\APEX\shared\external\include\SystemServices.h
+copy \p4\TRUNK\APEX\shared\external\src\SystemServices.cpp
+copy \p4\TRUNK\APEX\shared\external\include\targa.h
+copy \p4\TRUNK\APEX\shared\external\src\targa.cpp
+copy \p4\TRUNK\APEX\shared\external\include\telnet.h
+copy \p4\TRUNK\APEX\shared\external\src\telnet.cpp
+copy \p4\TRUNK\APEX\shared\external\include\UserMemAlloc.h
+copy \p4\TRUNK\APEX\shared\external\include\winmsg.h
+copy \p4\TRUNK\APEX\shared\external\src\winmsg.cpp
